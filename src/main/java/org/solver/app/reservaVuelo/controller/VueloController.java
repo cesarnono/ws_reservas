214a5facc03d6005a23ea03beef2439724398982
cesaraguirrevega <cesarnono2000@gmail.com>
2018-02-19 21:34:17 -0500

    funcionalidad de consultar vuelos

--- src/main/java/org/solver/app/reservaVuelo/controller/VueloController.java
+++ src/main/java/org/solver/app/reservaVuelo/controller/VueloController.java
@@ -13,7 +13,9 @@ import org.solver.app.reservaVuelo.entity.TarifaVuelo;
 import org.solver.app.reservaVuelo.entity.Vuelo;
 import org.solver.app.reservaVuelo.service.VueloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,22 +28,30 @@ public class VueloController {
     @Autowired
     VueloService vueloService;
     
-    @GetMapping("/consultarvuelos")
-    public List<Vuelo> consultarVuelos(){
-        Vuelo vuelo = new Vuelo(); 
+    @CrossOrigin
+    @PostMapping("/consultarvuelos")
+    public List<Vuelo> consultarVuelos(@RequestBody Vuelo vuelo){
+        /*Vuelo vuelo = new Vuelo(); 
         TarifaVuelo tarifaVuelo = new TarifaVuelo();
         tarifaVuelo.setCiudadOrigen("Bogota");
         tarifaVuelo.setCiudadDestino("Cúcuta");                
         vuelo.setTarifaVuelo(tarifaVuelo);
         vuelo.setFecha(Calendar.getInstance());
-        vuelo.getFecha().add(Calendar.DAY_OF_YEAR, 2);
-        return vueloService.consultarVuelos(vuelo);
+        vuelo.getFecha().add(Calendar.DAY_OF_YEAR, 2);*/
+        return vueloService.consultarVuelos(vuelo); 
     }
     
     
     @GetMapping("/vuelos")
     public List<Vuelo> consultarTodosVuelos(){
         return vueloService.consultarTodosVuelos();
+    }
+    
+    
+    @CrossOrigin
+    @GetMapping("/tarifas")    
+    public List<TarifaVuelo> consultarTodosTafifas(){
+        return vueloService.consultarTodosTarifas();
     }
 
 }
--- src/main/java/org/solver/app/reservaVuelo/service/VueloService.java
+++ src/main/java/org/solver/app/reservaVuelo/service/VueloService.java
@@ -7,7 +7,9 @@
 package org.solver.app.reservaVuelo.service;
 
 import java.util.List;
+import org.solver.app.reservaVuelo.Repository.TarifaVueloRepository;
 import org.solver.app.reservaVuelo.Repository.VueloRepository;
+import org.solver.app.reservaVuelo.entity.TarifaVuelo;
 import org.solver.app.reservaVuelo.entity.Vuelo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,11 +23,18 @@ public class VueloService {
   
     @Autowired
     VueloRepository vueloRepository;
+    @Autowired
+    TarifaVueloRepository tarifaVueloRepository;
+    
     public List<Vuelo> consultarVuelos(Vuelo vuelo){
         return vueloRepository.consultarVueloCriterio(vuelo.getTarifaVuelo().getCiudadOrigen(),vuelo.getTarifaVuelo().getCiudadDestino(),vuelo.getFecha());
     }
     
     public List<Vuelo> consultarTodosVuelos(){
         return vueloRepository.findAll();
+    }
+    
+    public List<TarifaVuelo> consultarTodosTarifas(){
+        return tarifaVueloRepository.findAll();
     }
 }
